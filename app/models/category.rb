3cb41d7fd8bd6c8c8a7d786629ea9b0b34488ac6
thanghd-2920 <ho.duc.thang@sun-asterisk.com>
2023-12-15 13:15:54 +0700

    remove model category

--- app/models/category.rb
+++
@@ -1,5 +0,0 @@
-class Category < ApplicationRecord
-  acts_as_paranoid
-
-  has_many :posts
-end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   acts_as_paranoid
-  enum category: {news: 0, finance: 1}
+  enum category: { news: 0, finance: 1 }
 
   belongs_to :admin
 end
