985cecfafedff9e2db0431978968d5f12c2d46a8
thanghd-2920 <ho.duc.thang@sun-asterisk.com>
2025-08-04 21:37:49 +0700

    update destroy

--- config/initializers/cookie_config.rb
+++ config/initializers/cookie_config.rb
@@ -3,8 +3,8 @@ Rails.application.config.to_prepare do
   # Định nghĩa cấu hình cookie
   COOKIE_OPTIONS = {
     httponly: true, # Ngăn chặn truy cập từ JavaScript
-    secure: Rails.env.production?, # Chỉ gửi cookie qua HTTPS trong môi trường production
-    same_site: Rails.env.production? ? :none : :lax,
+    secure: Rails.env.production? || ENV['FORCE_HTTPS'] == 'true', # Chỉ gửi cookie qua HTTPS trong môi trường production
+    same_site: (Rails.env.production? || ENV['FORCE_HTTPS'] == 'true') ? :none : :lax,
     expires: 7.days.from_now # Thời gian sống của cookie
   }.freeze
 end
